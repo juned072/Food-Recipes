208870afd99df8e3054127ba23732228bcc36bf4
juned072 <junedansari9172@gmail.com>
2024-07-24 14:51:34 +0530

    Add header to Home component and improve layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import Card from "../components/Card";
 const Home = () => {
   return (
     <div className="max-w-screen-lg mx-auto my-5">
+      <h2 className="text-center font-bold text-3xl my-8">
+        Our <span className="text-red-500">Menu</span>
+      </h2>
       <div className="flex justify-center items-center">
         <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
           {Data.map((item) => {
