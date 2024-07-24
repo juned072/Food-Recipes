e43370819c21587c498fd04e5fe0cf9a1dc62bfc
juned072 <junedansari9172@gmail.com>
2024-07-24 12:36:23 +0530

    Added 'Data.js' file containing an array of foods objects with details such as id, title, image, rating, price, and description

---
+++ src/database/Data.js
@@ -0,0 +1,124 @@
+const Data = [
+    {
+        "id": 1,
+        "title": "Pizza Crunchy",
+        "image": "https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?t=st=1718773917~exp=1718777517~hmac=8e4fcd0117ce295a10aa704dcfa8e74279b7b556fec1265b2b31f49fd9d7983c&w=740",
+        "rating": "⭐⭐⭐⭐",
+        "price": 99,
+        "description": "Crunchy crust, flavorful sauce, and melted mozzarella cheese"
+    },
+    {
+        "id": 3,
+        "title": "Hawaiian Special",
+        "image": "https://img.freepik.com/free-photo/pizza-with-pineapple-ham-table_2829-13264.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 95,
+        "description": "Ham, pineapple, and mozzarella on a crispy crust"
+    },
+    {
+        "id": 4,
+        "title": "Cheese Overload",
+        "image": "https://img.freepik.com/free-photo/cheese-pizza-isolated-white_1339-17703.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 10,
+        "description": "A blend of four cheeses on a crispy crust"
+    },
+    {
+        "id": 5,
+        "title": "Spicy Jalapeno",
+        "image": "https://img.freepik.com/free-photo/pizza-with-jalapenos-peppers-cheese-wooden-table_141793-57.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 90,
+        "description": "Spicy jalapenos with mozzarella and tomato sauce"
+    },
+    {
+        "id": 6,
+        "title": "Mushroom Mania",
+        "image": "https://img.freepik.com/free-photo/pizza-with-mushrooms-spinach-wooden-board_2829-13428.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 92,
+        "description": "Loaded with mushrooms and extra cheese"
+    },
+    {
+        "id": 7,
+        "title": "Meat Lovers",
+        "image": "https://img.freepik.com/free-photo/meat-lovers-pizza-with-sausages-pepperoni-bacon_144627-40130.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 11,
+        "description": "Pepperoni, sausage, bacon, and ham on a crispy crust"
+    },
+    {
+        "id": 8,
+        "title": "Pesto Passion",
+        "image": "https://img.freepik.com/free-photo/pizza-with-pesto-sauce-spinach-cheese-top-view_2829-10823.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 98,
+        "description": "Pesto sauce, mozzarella, and fresh spinach"
+    },
+    {
+        "id": 9,
+        "title": "Four Seasons",
+        "image": "https://img.freepik.com/free-photo/four-seasons-pizza-with-four-different-toppings_144627-40143.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 12,
+        "description": "Four different toppings for every quarter"
+    },
+    {
+        "id": 10,
+        "title": "Garlic Chicken",
+        "image": "https://img.freepik.com/free-photo/garlic-chicken-pizza-wooden-table_2829-10798.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 10,
+        "description": "Grilled chicken, garlic sauce, and mozzarella"
+    },
+    {
+        "id": 11,
+        "title": "Tuna Delight",
+        "image": "https://img.freepik.com/free-photo/pizza-with-tuna-onions-cheese-wooden-board_2829-11676.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 97,
+        "description": "Tuna, red onions, and mozzarella on a crispy crust"
+    },
+    {
+        "id": 12,
+        "title": "Shrimp Scampi",
+        "image": "https://img.freepik.com/free-photo/shrimp-scampi-pizza-wooden-table_2829-10743.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 11,
+        "description": "Shrimp, garlic sauce, and mozzarella on a crispy crust"
+    },
+    {
+        "id": 13,
+        "title": "Buffalo Chicken",
+        "image": "https://img.freepik.com/free-photo/buffalo-chicken-pizza-wooden-table_2829-10645.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 112,
+        "description": "Spicy buffalo chicken and mozzarella on a crispy crust"
+    },
+    {
+        "id": 14,
+        "title": "Spinach & Ricotta",
+        "image": "https://img.freepik.com/free-photo/pizza-with-spinach-ricotta-cheese-wooden-table_2829-10789.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 93,
+        "description": "Fresh spinach and ricotta cheese on a crispy crust"
+    },
+    {
+        "id": 15,
+        "title": "Truffle Special",
+        "image": "https://img.freepik.com/free-photo/pizza-with-truffle-oil-cheese-wooden-board_2829-10821.jpg",
+        "rating": "⭐⭐⭐⭐⭐",
+        "price": 130,
+        "description": "Truffle oil, mushrooms, and mozzarella"
+    },
+    {
+        "id": 16,
+        "title": "Eggplant Parm",
+        "image": "https://img.freepik.com/free-photo/eggplant-parmesan-pizza-wooden-table_2829-10689.jpg",
+        "rating": "⭐⭐⭐⭐",
+        "price": 95,
+        "description": "Breaded eggplant, parmesan, and mozzarella on a crispy crust"
+    },
+
+];
+export default Data;
