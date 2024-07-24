cee1c896684f4063c9aaf5af2d6d32433d6b6049
juned072 <junedansari9172@gmail.com>
2024-07-24 12:14:56 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
