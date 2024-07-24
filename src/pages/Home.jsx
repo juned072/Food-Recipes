be84dc2a366d31a141a3fa9dc40270329c84d4df
juned072 <junedansari9172@gmail.com>
2024-07-24 14:27:15 +0530

    Implement Home component to display pizza menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Data from "../database/Data";
+import Card from "../components/Card";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="max-w-screen-lg bg-red-400 mx-auto my-5">
+      <div className="grid grid-cols-3 gap-5">
+        {Data.map((item) => {
+          return <Card key={item.id} item={item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
