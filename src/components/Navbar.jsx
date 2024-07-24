ccfb7f3abed526c518ef7f674d2d39be05c98bca
juned072 <junedansari9172@gmail.com>
2024-07-24 12:22:01 +0530

    linked  with pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 shadow-md">
       <div>
-        <h1 className="text-3xl font-bold text-slate-800">Food Recipes</h1>
+        <Link to={"/"}>
+          <h1 className="text-3xl font-bold text-slate-800">Food Recipes</h1>
+        </Link>
       </div>
       <div>
-        <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
-          Home
-        </button>
-        <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
-          Cart
-        </button>
-        <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
-          Wishlist
-        </button>
+        <Link to={"/"}>
+          <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
+            Home
+          </button>
+        </Link>
+        <Link to={"/wishlist"}>
+          <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
+            Wishlist
+          </button>
+        </Link>
+        <Link to={"/cart"}>
+          <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
+            Cart
+          </button>
+        </Link>
       </div>
     </nav>
   );
