11a0b09d0245f751c08177f07e58937e0b659f5f
juned072 <junedansari9172@gmail.com>
2024-07-24 14:48:15 +0530

    Update Home component for improved layout and responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Data from "../database/Data";
 import Card from "../components/Card";
 const Home = () => {
   return (
-    <div className="max-w-screen-lg bg-red-400 mx-auto my-5">
-      <div className="grid grid-cols-3 gap-5">
-        {Data.map((item) => {
-          return <Card key={item.id} item={item} />;
-        })}
+    <div className="max-w-screen-lg mx-auto my-5">
+      <div className="flex justify-center items-center">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
+          {Data.map((item) => {
+            return <Card key={item.id} item={item} />;
+          })}
+        </div>
       </div>
     </div>
   );
