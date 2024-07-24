a9f25aa1e527359df5ab976846870108119eb698
juned072 <junedansari9172@gmail.com>
2024-07-24 12:09:15 +0530

    logo name and nav link added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-4 shadow-md">
+      <div>
+        <h1 className="text-3xl font-bold text-slate-800">Food Recipes</h1>
+      </div>
+      <div>
+        <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
+          Cart
+        </button>
+        <button className="p-2 mx-2 rounded-md bg-white hover:bg-red-500 border hover:text-white">
+          Wishlist
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
