0dde6a4dc8821b715f266322f90c0e3e81f76fe9
juned072 <junedansari9172@gmail.com>
2024-07-25 10:00:53 +0530

    recommended component are get into the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Data from "../database/Data";
 import Card from "../components/Card";
+import Recommended from "../components/Recommended";
+
 const Home = () => {
   return (
     <div className="max-w-screen-lg mx-auto my-5">
       <h2 className="text-center font-bold text-3xl my-8">
         Our <span className="text-red-500">Menu</span>
       </h2>
+      <div className="p-2">
+        <Recommended />
+      </div>
       <div className="flex justify-center items-center">
         <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-2 gap-5 p-2">
           {Data.map((item) => {
