85f5b7c4910dd270940e1f435ccd8a439752afe4
juned072 <junedansari9172@gmail.com>
2024-07-25 09:53:49 +0530

    Data file updated with proper content and images

--- src/database/Data.js
+++ src/database/Data.js
@@ -13,15 +13,15 @@ const Data = [
         "image": "https://img.freepik.com/free-photo/pizza-with-pineapple-ham-table_2829-13264.jpg",
         "rating": "⭐⭐⭐⭐",
         "price": 95,
-        "description": "Enjoy the sweet and savory taste of our Hawaiian Special pizza, featuring ham, pineapple, and mozzarella on a crispy crust. The dough is kneaded and left to rise for a few hours, ensuring a light and airy texture. A rich tomato sauce is spread evenly over the crust, followed by layers of ham and juicy pineapple chunks. Finally, a generous amount of mozzarella cheese is added before baking. This pizza offers a delightful combination of flavors and textures in every bite."
+        "description": "Enjoy the sweet and savory taste of our Hawaiian Special, featuring ham, pineapple, and mozzarella on a crispy crust. The dough is kneaded and left to rise for a few hours, ensuring a light and airy texture. A rich tomato sauce is spread evenly over the crust, followed by layers of ham and juicy pineapple chunks. Finally, a generous amount of mozzarella cheese is added before baking. This offers a delightful combination of flavors and textures in every bite."
     },
     {
         "id": 4,
         "title": "Cheese Overload",
-        "image": "https://img.freepik.com/free-photo/cheese-pizza-isolated-white_1339-17703.jpg",
+        "image": "https://img.freepik.com/free-photo/delicious-melted-cheese-snacks_23-2149274826.jpg?t=st=1721880830~exp=1721884430~hmac=c9e8c6eab02f4c8f4f2dea112e447e1516dc4570b643fbc74f6d303b503027bc&w=740",
         "rating": "⭐⭐⭐⭐⭐",
         "price": 10,
-        "description": "Indulge in the ultimate cheese lover's dream with our Cheese Overload pizza. This pizza features a blend of four cheeses on a crispy crust. The dough is prepared and rolled out to a thin base, then topped with a mix of mozzarella, cheddar, Parmesan, and blue cheese. Each cheese brings its unique flavor, creating a rich and creamy experience. The pizza is baked until the cheese is melted and bubbly, and the crust is golden and crisp. Perfect for those who can't get enough cheese!"
+        "description": "Indulge in the ultimate cheese lover's dream with our Cheese Overload. This features a blend of four cheeses on a crispy crust. The dough is prepared and rolled out to a thin base, then topped with a mix of mozzarella, cheddar, Parmesan, and blue cheese. Each cheese brings its unique flavor, creating a rich and creamy experience. The is baked until the cheese is melted and bubbly, and the crust is golden and crisp. Perfect for those who can't get enough cheese!"
     },
     {
         "id": 5,
@@ -29,7 +29,7 @@ const Data = [
         "image": "https://img.freepik.com/free-photo/pizza-with-jalapenos-peppers-cheese-wooden-table_141793-57.jpg",
         "rating": "⭐⭐⭐⭐",
         "price": 90,
-        "description": "Add some heat to your meal with our Spicy Jalapeno pizza. This pizza combines spicy jalapenos with mozzarella and tomato sauce on a crispy crust. The dough is kneaded and allowed to rise, ensuring a perfect base. A spicy tomato sauce is spread over the dough, followed by slices of fresh jalapenos and a generous amount of mozzarella cheese. The pizza is baked until the cheese is melted and the crust is crispy. Each bite delivers a fiery kick that is balanced by the creamy cheese."
+        "description": "Add some heat to your meal with our Spicy Jalapeno. This combines spicy jalapenos with mozzarella and tomato sauce on a crispy crust. The dough is kneaded and allowed to rise, ensuring a perfect base. A spicy tomato sauce is spread over the dough, followed by slices of fresh jalapenos and a generous amount of mozzarella cheese. The is baked until the cheese is melted and the crust is crispy. Each bite delivers a fiery kick that is balanced by the creamy cheese."
     },
     {
         "id": 6,
@@ -41,11 +41,11 @@ const Data = [
     },
     {
         "id": 7,
-        "title": "Meat Lovers",
-        "image": "https://img.freepik.com/free-photo/meat-lovers-pizza-with-sausages-pepperoni-bacon_144627-40130.jpg",
+        "title": "Burger",
+        "image": "https://img.freepik.com/free-photo/view-ready-eat-delicious-meal-go_23-2151431747.jpg?t=st=1721881133~exp=1721884733~hmac=62552f1a2e8e2f0b78878dbc227d1312f89ecbc9ab1c7e9ae396ed4bc587c532&w=740",
         "rating": "⭐⭐⭐⭐",
         "price": 11,
-        "description": "Satisfy your carnivorous cravings with our Meat Lovers pizza, featuring pepperoni, sausage, bacon, and ham on a crispy crust. The dough is prepared and left to rise, ensuring a perfect base. A rich tomato sauce is spread over the dough, followed by layers of pepperoni, sausage, bacon, and ham. A generous amount of mozzarella cheese is added before baking. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite is packed with savory meat flavors."
+        "description": "Satisfy your carnivorous cravings with our Burger, featuring pepperoni, sausage, bacon, and ham on a crispy crust. The dough is prepared and left to rise, ensuring a perfect base. A rich tomato sauce is spread over the dough, followed by layers of pepperoni, sausage, bacon, and ham. A generous amount of mozzarella cheese is added before baking. The is baked until the cheese is melted and the crust is golden and crisp. Each bite is packed with savory meat flavors."
     },
     {
         "id": 8,
@@ -53,15 +53,15 @@ const Data = [
         "image": "https://img.freepik.com/free-photo/pizza-with-pesto-sauce-spinach-cheese-top-view_2829-10823.jpg",
         "rating": "⭐⭐⭐⭐⭐",
         "price": 98,
-        "description": "Experience the fresh and vibrant flavors of our Pesto Passion pizza. This pizza features a base of homemade pesto sauce, mozzarella, and fresh spinach. The dough is kneaded and left to rise, then rolled out to a thin base. A generous layer of pesto sauce is spread over the dough, followed by fresh spinach leaves and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is crispy. Each bite is a burst of fresh and herby flavors."
+        "description": "Experience the fresh and vibrant flavors of our Pesto Passion. This features a base of homemade pesto sauce, mozzarella, and fresh spinach. The dough is kneaded and left to rise, then rolled out to a thin base. A generous layer of pesto sauce is spread over the dough, followed by fresh spinach leaves and mozzarella cheese. The is baked until the cheese is melted and the crust is crispy. Each bite is a burst of fresh and herby flavors."
     },
     {
         "id": 9,
-        "title": "Four Seasons",
-        "image": "https://img.freepik.com/free-photo/four-seasons-pizza-with-four-different-toppings_144627-40143.jpg",
+        "title": "Four Seasons Pizza",
+        "image": "https://img.freepik.com/free-photo/italian-food-composition-with-pizza-middle_23-2147686548.jpg?t=st=1721880704~exp=1721884304~hmac=37e47fe89fab76b84cf5d84dfe04b61b7f58ff663a4c249800232d11450c5228&w=740",
         "rating": "⭐⭐⭐⭐",
         "price": 12,
-        "description": "Enjoy a variety of flavors with our Four Seasons pizza, featuring four different toppings for every quarter. The dough is prepared and rolled out to a thin base, then divided into four sections. Each section is topped with a different combination of ingredients, such as ham and mushrooms, artichokes and olives, pepperoni and peppers, and cheese and tomatoes. The pizza is baked until the cheese is melted and the crust is crispy. Each bite offers a unique taste experience."
+        "description": "Enjoy a variety of flavors with our Four Seasons, featuring four different toppings for every quarter. The dough is prepared and rolled out to a thin base, then divided into four sections. Each section is topped with a different combination of ingredients, such as ham and mushrooms, artichokes and olives, pepperoni and peppers, and cheese and tomatoes. The is baked until the cheese is melted and the crust is crispy. Each bite offers a unique taste experience."
     },
     {
         "id": 10,
@@ -69,7 +69,7 @@ const Data = [
         "image": "https://img.freepik.com/free-photo/garlic-chicken-pizza-wooden-table_2829-10798.jpg",
         "rating": "⭐⭐⭐⭐⭐",
         "price": 10,
-        "description": "Savor the rich and savory flavors of our Garlic Chicken pizza. This pizza features grilled chicken, garlic sauce, and mozzarella on a crispy crust. The dough is kneaded and allowed to rise, then rolled out to a thin base. A creamy garlic sauce is spread over the dough, followed by slices of grilled chicken and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite delivers a perfect balance of garlic and chicken flavors."
+        "description": "Savor the rich and savory flavors of our Garlic Chicken. This features grilled chicken, garlic sauce, and mozzarella on a crispy crust. The dough is kneaded and allowed to rise, then rolled out to a thin base. A creamy garlic sauce is spread over the dough, followed by slices of grilled chicken and mozzarella cheese. The is baked until the cheese is melted and the crust is golden and crisp. Each bite delivers a perfect balance of garlic and chicken flavors."
     },
     {
         "id": 11,
@@ -85,39 +85,31 @@ const Data = [
         "image": "https://img.freepik.com/free-photo/shrimp-scampi-pizza-wooden-table_2829-10743.jpg",
         "rating": "⭐⭐⭐⭐",
         "price": 11,
-        "description": "Indulge in the rich and savory flavors of our Shrimp Scampi pizza. This pizza features shrimp, garlic sauce, and mozzarella on a crispy crust. The dough is kneaded and allowed to rise, then rolled out to a thin base. A creamy garlic sauce is spread over the dough, followed by succulent shrimp and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite is a delicious blend of garlic and shrimp flavors."
+        "description": "Indulge in the rich and savory flavors of our Shrimp Scampi. This features shrimp, garlic sauce, and mozzarella on a crispy crust. The dough is kneaded and allowed to rise, then rolled out to a thin base. A creamy garlic sauce is spread over the dough, followed by succulent shrimp and mozzarella cheese. The is baked until the cheese is melted and the crust is golden and crisp. Each bite is a delicious blend of garlic and shrimp flavors."
     },
     {
         "id": 13,
-        "title": "Buffalo Chicken",
-        "image": "https://img.freepik.com/free-photo/buffalo-chicken-pizza-wooden-table_2829-10645.jpg",
-        "rating": "⭐⭐⭐⭐⭐",
-        "price": 112,
-        "description": "Spice up your meal with our Buffalo Chicken pizza, featuring spicy buffalo chicken and mozzarella on a crispy crust. The dough is prepared and left to rise, then rolled out to a thin base. A spicy buffalo sauce is spread over the dough, followed by pieces of buffalo chicken and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite delivers a fiery kick balanced by the creamy cheese."
-    },
-    {
-        "id": 14,
         "title": "Spinach & Ricotta",
         "image": "https://img.freepik.com/free-photo/pizza-with-spinach-ricotta-cheese-wooden-table_2829-10789.jpg",
         "rating": "⭐⭐⭐⭐",
         "price": 93,
-        "description": "Delight in the fresh and creamy flavors of our Spinach & Ricotta pizza. This pizza features fresh spinach and ricotta cheese on a crispy crust. The dough is kneaded and left to rise, then rolled out to a thin base. A rich tomato sauce is spread over the dough, followed by fresh spinach leaves and dollops of ricotta cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite is a celebration of fresh and creamy flavors."
+        "description": "Delight in the fresh and creamy flavors of our Spinach & Ricotta. This features fresh spinach and ricotta cheese on a crispy crust. The dough is kneaded and left to rise, then rolled out to a thin base. A rich tomato sauce is spread over the dough, followed by fresh spinach leaves and dollops of ricotta cheese. The is baked until the cheese is melted and the crust is golden and crisp. Each bite is a celebration of fresh and creamy flavors."
     },
     {
-        "id": 15,
+        "id": 14,
         "title": "Truffle Special",
         "image": "https://img.freepik.com/free-photo/pizza-with-truffle-oil-cheese-wooden-board_2829-10821.jpg",
         "rating": "⭐⭐⭐⭐⭐",
         "price": 130,
-        "description": "Indulge in the luxurious flavors of our Truffle Special pizza. This pizza features truffle oil, mushrooms, and mozzarella on a crispy crust. The dough is prepared and left to rise, then rolled out to a thin base. A generous drizzle of truffle oil is spread over the dough, followed by sautéed mushrooms and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite delivers a rich and aromatic truffle experience."
+        "description": "Indulge in the luxurious flavors of our Truffle Special. This features truffle oil, mushrooms, and mozzarella on a crispy crust. The dough is prepared and left to rise, then rolled out to a thin base. A generous drizzle of truffle oil is spread over the dough, followed by sautéed mushrooms and mozzarella cheese. The is baked until the cheese is melted and the crust is golden and crisp. Each bite delivers a rich and aromatic truffle experience."
     },
     {
-        "id": 16,
+        "id": 15,
         "title": "Eggplant Parm",
         "image": "https://img.freepik.com/free-photo/eggplant-parmesan-pizza-wooden-table_2829-10689.jpg",
         "rating": "⭐⭐⭐⭐",
         "price": 95,
-        "description": "Enjoy the classic flavors of our Eggplant Parm pizza. This pizza features breaded eggplant, parmesan, and mozzarella on a crispy crust. The dough is prepared and left to rise, then rolled out to a thin base. A rich tomato sauce is spread over the dough, followed by slices of breaded eggplant and a generous amount of parmesan and mozzarella cheese. The pizza is baked until the cheese is melted and the crust is golden and crisp. Each bite offers a delicious blend of eggplant and cheese flavors."
+        "description": "Enjoy the classic flavors of our Eggplant Parm. This features breaded eggplant, parmesan, and mozzarella on a crispy crust. The dough is prepared and left to rise, then rolled out to a thin base. A rich tomato sauce is spread over the dough, followed by slices of breaded eggplant and a generous amount of parmesan and mozzarella cheese. The is baked until the cheese is melted and the crust is golden and crisp. Each bite offers a delicious blend of eggplant and cheese flavors."
     },
 ];
 
