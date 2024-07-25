860e4fca7f838edc9051961816348ad775408790
juned072 <junedansari9172@gmail.com>
2024-07-25 10:57:51 +0530

    filter with category function create and applied on button

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-const Recommended = () => {
+const Recommended = ({ setFilter }) => {
   return (
     <div>
-      <button className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white">
+      <button
+        onClick={() => setFilter("All")}
+        className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white"
+      >
         All
       </button>
-      <button className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white">
+      <button
+        onClick={() => setFilter("veg")}
+        className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white"
+      >
         Veg
       </button>
-      <button className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white">
+      <button
+        onClick={() => setFilter("non-veg")}
+        className="p-2 mx-2 rounded-md border hover:bg-red-500 hover:text-white"
+      >
         Non-veg
       </button>
     </div>
