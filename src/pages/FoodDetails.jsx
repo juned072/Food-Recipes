9894389e907bc94b681fdd218d18d5078333833a
juned072 <junedansari9172@gmail.com>
2024-07-28 13:51:50 +0530

    react spinner added

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -5,6 +5,7 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
 import { ContextApi } from "../context/ContextApi";
+import { PulseLoader } from "react-spinners";
 
 const FoodDetails = () => {
   const { id } = useParams();
@@ -17,63 +18,81 @@ const FoodDetails = () => {
 
   useEffect(() => {
     const fetchedItem = getItemById(id);
-    setItem(fetchedItem);
+    setTimeout(() => {
+      setItem(fetchedItem);
+    }, 1000);
   }, [id]);
 
   return (
     <div className="max-w-screen-lg mx-auto p-5">
-      <div className="flex mb-4">
-        <Link to={"/"}>
-          <div className="bg-red-600 hover:bg-red-700 transition-colors duration-300 w-8 h-8 flex items-center justify-center rounded-full shadow-lg">
-            <IoMdArrowRoundBack className="text-white text-xl" />
-          </div>
-        </Link>
-      </div>
-      <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 md:space-x-5">
-        <div className="md:w-1/2">
-          <img
-            src={item.image}
-            alt={item.title}
-            className="rounded-md w-full object-cover md:h-96 h-72"
+      {!item ? (
+        <div className="flex justify-center items-center h-96">
+          <PulseLoader
+            color="#f42222"
+            margin={5}
+            size={10}
+            speedMultiplier={1}
           />
         </div>
-        <div className="md:w-1/2">
-          <h3 className="text-2xl font-semibold py-2">{item.title}</h3>
-          <p className="block mb-2">
-            <span className="mr-1">Rating:</span> {item.rating}
-          </p>
-          <p className="mb-2">
-            <span className="mr-1">Availability:</span>
-            <span className={item.inStock ? "text-green-600" : "text-red-600"}>
-              {item.inStock ? "In Stock" : "Out of Stock"}
-            </span>
-          </p>
-          <p className="mb-2">
-            Category:<span className="ml-1 text-gray-500">{item.category}</span>
-          </p>
-          <p className="mb-4">
-            Price:
-            <span className="text-xl font-bold text-red-500">
-              ${item.price}
-            </span>
-          </p>
-          <p className="mb-4 text-gray-800">{item.description}</p>
-          <div className="flex space-x-4">
-            <button
-              onClick={() => addToWishlist(item)}
-              className="flex items-center bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-600 transition-all duration-300"
-            >
-              <CiHeart className="mr-2" /> Wishlist
-            </button>
-            <button
-              onClick={() => addToCart(item)}
-              className="flex items-center bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 transition-all duration-300"
-            >
-              <IoBagHandleOutline className="mr-2" /> Add to Cart
-            </button>
+      ) : (
+        <div>
+          <div className="flex mb-4">
+            <Link to={"/"}>
+              <div className="bg-red-600 hover:bg-red-700 transition-colors duration-300 w-8 h-8 flex items-center justify-center rounded-full shadow-lg">
+                <IoMdArrowRoundBack className="text-white text-xl" />
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 md:space-x-5">
+            <div className="md:w-1/2">
+              <img
+                src={item.image}
+                alt={item.title}
+                className="rounded-md w-full object-cover md:h-96 h-72"
+              />
+            </div>
+            <div className="md:w-1/2">
+              <h3 className="text-2xl font-semibold py-2">{item.title}</h3>
+              <p className="block mb-2">
+                <span className="mr-1">Rating:</span> {item.rating}
+              </p>
+              <p className="mb-2">
+                <span className="mr-1">Availability:</span>
+                <span
+                  className={item.inStock ? "text-green-600" : "text-red-600"}
+                >
+                  {item.inStock ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
+              <p className="mb-2">
+                Category:
+                <span className="ml-1 text-gray-500">{item.category}</span>
+              </p>
+              <p className="mb-4">
+                Price:
+                <span className="text-xl font-bold text-red-500">
+                  ${item.price}
+                </span>
+              </p>
+              <p className="mb-4 text-gray-800">{item.description}</p>
+              <div className="flex space-x-4">
+                <button
+                  onClick={() => addToWishlist(item)}
+                  className="flex items-center bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-600 transition-all duration-300"
+                >
+                  <CiHeart className="mr-2" /> Wishlist
+                </button>
+                <button
+                  onClick={() => addToCart(item)}
+                  className="flex items-center bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 transition-all duration-300"
+                >
+                  <IoBagHandleOutline className="mr-2" /> Add to Cart
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
